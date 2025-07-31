a0f592c073cbea348e3d4b02af8a82ad1548643e
Vlad <nowornever969@gmail.com>
2025-07-31 21:45:53 +0300

    add modern-normalize and fix width

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./components/App/App.jsx";
+import "modern-normalize";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
