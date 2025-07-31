26974aadbb3cc7a17ed01482ac25d8785d1b1d7d
Vlad <nowornever969@gmail.com>
2025-07-31 21:13:37 +0300

    add AppBar + Burger Menu (only mobile)

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,3 +1,50 @@
+import s from "./AppBar.module.css";
+import sprite from "../../assets/icons/sprite.svg";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import BurgerMenu from "../BurgerMenu/BurgerMenu";
+
 export default function AppBar() {
-  return <h1>Привіт</h1>;
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleModal = () => {
+    if (isModalOpen) {
+      setIsModalOpen(false);
+      setTimeout(() => setShowMenu(false), 300);
+    } else {
+      setShowMenu(true);
+      setTimeout(() => setIsModalOpen(true), 0);
+    }
+  };
+
+  return (
+    <header className={s.header}>
+      <div className={s.container}>
+        <Link to="/" className={s.logoLink}>
+          <svg className={s.logo}>
+            <use href={`${sprite}#icon-logo`} />
+          </svg>
+        </Link>
+
+        <div className={s.iconsContainer}>
+          <svg className={s.searchIcon}>
+            <use href={`${sprite}#icon-search `} />
+          </svg>
+
+          <button
+            className={`${s.burgerBtn} ${isModalOpen ? s.open : ""}`}
+            onClick={toggleModal}
+            aria-label="Open mobile menu"
+          >
+            <span className={s.burgerLine}></span>
+            <span className={s.burgerLine}></span>
+            <span className={s.burgerLine}></span>
+          </button>
+        </div>
+      </div>
+
+      {showMenu && <BurgerMenu isOpen={isModalOpen} onClose={toggleModal} />}
+    </header>
+  );
 }
---
+++ src/components/BurgerMenu/BurgerMenu.jsx
@@ -0,0 +1,99 @@
+import s from "./BurgerMenu.module.css";
+import sprite from "../../assets/icons/sprite.svg";
+import { NavLink } from "react-router-dom";
+import { useEffect } from "react";
+
+export default function BurgerMenu({ isOpen, onClose }) {
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "";
+    }
+
+    return () => {
+      document.body.style.overflow = "";
+    };
+  }, [isOpen]);
+
+  const handleBackdropClick = (e) => {
+    if (e.target === e.currentTarget) onClose();
+  };
+
+  return (
+    <div
+      className={`${s.backdrop} ${isOpen ? s.open : ""}`}
+      onClick={handleBackdropClick}
+    >
+      <div className={s.modal}>
+        <div>
+          <ul className={s.list}>
+            <li>
+              <NavLink to="/" className={s.link} onClick={onClose}>
+                Про нас
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/" className={s.link} onClick={onClose}>
+                Тварини
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/" className={s.link} onClick={onClose}>
+                Контакти
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/" className={s.link} onClick={onClose}>
+                Блог
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/" className={s.link} onClick={onClose}>
+                Відгуки
+              </NavLink>
+            </li>
+          </ul>
+
+          <div className={s.btnContainer}>
+            <button className={s.btnEnter}>
+              Увійти
+              <svg className={s.entranceIcon}>
+                <use href={`${sprite}#icon-entrance`} />
+              </svg>
+            </button>
+            <button className={s.btnDonation}>
+              Підтримати
+              <svg className={s.coinIcon}>
+                <use href={`${sprite}#icon-coin`} />
+              </svg>
+            </button>
+          </div>
+        </div>
+
+        <ul className={s.mediaList}>
+          <li>
+            <svg className={s.mediaIcon}>
+              <use href={`${sprite}#icon-telegram`} />
+            </svg>
+          </li>
+          <li>
+            <svg className={s.mediaIcon}>
+              <use href={`${sprite}#icon-instagram`} />
+            </svg>
+          </li>
+          <li>
+            <svg className={s.mediaIcon}>
+              <use href={`${sprite}#icon-facebook`} />
+            </svg>
+          </li>
+          <li>
+            <svg className={s.mediaIcon}>
+              <use href={`${sprite}#icon-youtube`} />
+            </svg>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
