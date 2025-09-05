04121604d3ae5add88307944b6ade8e11361f448
Vlad <nowornever969@gmail.com>
2025-09-05 15:12:09 +0300

    add backend (Pets List, local connection)

--- src/components/Pets/Pets.jsx
+++ src/components/Pets/Pets.jsx
@@ -1,9 +1,30 @@
+import { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import s from "./Pets.module.css";
 import PetsList from "../PetsList/PetsList";
-import petsData from "../../../petsData.json";
 
 export default function Pets() {
+  const [petsData, setPetsData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchPets = async () => {
+      try {
+        const res = await fetch("http://localhost:5000/api/pets");
+        const data = await res.json();
+        setPetsData(data);
+      } catch (err) {
+        console.error("Error fetching pets:", err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchPets();
+  }, []);
+
+  if (loading) return <p>Завантаження...</p>;
+
   return (
     <div className={s.container}>
       <h2 className={s.title}>Ці хвостики чекають на вас</h2>
