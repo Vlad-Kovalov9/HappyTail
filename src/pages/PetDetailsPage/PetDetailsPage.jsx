ffec4b5fdb21da148980cbdf34f7253edd4cb150
Vlad <nowornever969@gmail.com>
2025-09-16 19:32:12 +0300

    Finish PetDetailsPage (responsive design)

--- src/pages/PetDetailsPage/PetDetailsPage.jsx
+++ src/pages/PetDetailsPage/PetDetailsPage.jsx
@@ -1,7 +1,7 @@
 import s from "./PetDetailsPage.module.css";
 import sprite from "../../assets/icons/sprite.svg";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { NavLink, useParams } from "react-router-dom";
 
 export default function PetDetailsPage() {
   const { id } = useParams();
@@ -20,61 +20,84 @@ export default function PetDetailsPage() {
 
   return (
     <div className={s.container}>
-      <img src={pet.image} alt={`${pet.name} Фото`} className={s.image} />
-      <h2 className={s.name}>{pet.name}</h2>
-
-      <ul className={s.list}>
-        <li className={s.item}>
-          <p className={s.itemFeature}>
-            <svg className={s.icon}>
-              <use href={`${sprite}#icon-pets-gender`} />
-            </svg>
-            <span className={s.feature}>Стать</span>
-          </p>
-          <p className={s.itemInfo}>{pet.gender}</p>
-        </li>
-
-        <li className={s.item}>
-          <p className={s.itemFeature}>
-            <svg className={s.icon}>
-              <use href={`${sprite}#icon-pets-age`} />
-            </svg>
-            <span className={s.feature}>Вік</span>
-          </p>
-          <p className={s.itemInfo}>{pet.age}</p>
-        </li>
-
-        <li className={s.item}>
-          <p className={s.itemFeature}>
-            <svg className={s.icon}>
-              <use href={`${sprite}#icon-pets-location`} />
-            </svg>
-            <span className={s.feature}>Місто</span>
-          </p>
-          <p className={s.itemInfo}>{pet.city}</p>
-        </li>
-
-        <li className={s.item}>
-          <p className={s.itemFeature}>
-            <svg className={s.icon}>
-              <use href={`${sprite}#icon-pets-sterilization`} />
-            </svg>
-            <span className={s.feature}>Стерилізація</span>
-          </p>
-          <p className={s.itemInfo}>
-            {pet.sterilization ? "Проведено" : "Не проведено"}
-          </p>
-        </li>
-      </ul>
-
-      <h3 className={s.text}>Особливості поведінки</h3>
-      <p className={s.description}>{pet.description}</p>
-      <button className={s.button}>
-        Усиновити
-        <svg className={s.iconHeart}>
-          <use href={`${sprite}#icon-pets-heart`} />
+      <NavLink to="/pets" className={s.link}>
+        <svg className={s.iconBack}>
+          <use href={`${sprite}#icon-backPage`} />
         </svg>
-      </button>
+        <span>Назад до хвостиків</span>
+      </NavLink>
+      <div className={s.infoContainer}>
+        <img src={pet.image} alt={`${pet.name} Фото`} className={s.image} />
+        <div className={s.featureContainer}>
+          <h2 className={s.name}>{pet.name}</h2>
+
+          <ul className={s.list}>
+            <li className={s.item}>
+              <p className={s.itemFeature}>
+                <svg className={s.icon}>
+                  <use href={`${sprite}#icon-pets-gender`} />
+                </svg>
+                <span className={s.feature}>Стать</span>
+              </p>
+              <p className={s.itemInfo}>{pet.gender}</p>
+            </li>
+
+            <li className={s.item}>
+              <p className={s.itemFeature}>
+                <svg className={s.icon}>
+                  <use href={`${sprite}#icon-pets-age`} />
+                </svg>
+                <span className={s.feature}>Вік</span>
+              </p>
+              <p className={s.itemInfo}>{pet.age}</p>
+            </li>
+
+            <li className={s.item}>
+              <p className={s.itemFeature}>
+                <svg className={s.icon}>
+                  <use href={`${sprite}#icon-pets-location`} />
+                </svg>
+                <span className={s.feature}>Місто</span>
+              </p>
+              <p className={s.itemInfo}>{pet.city}</p>
+            </li>
+
+            <li className={s.item}>
+              <p className={s.itemFeature}>
+                <svg className={s.icon}>
+                  <use href={`${sprite}#icon-pets-sterilization`} />
+                </svg>
+                <span className={s.feature}>Стерилізація</span>
+              </p>
+              <p className={s.itemInfo}>
+                {pet.sterilization ? "Проведено" : "Не проведено"}
+              </p>
+            </li>
+          </ul>
+
+          <div className={s.hideMobile}>
+            <h3 className={s.text}>Особливості поведінки</h3>
+            <p className={s.description}>{pet.description}</p>
+            <button className={s.button}>
+              <span>Усиновити</span>
+              <svg className={s.iconHeart}>
+                <use href={`${sprite}#icon-pets-heart`} />
+              </svg>
+            </button>
+          </div>
+        </div>
+
+        <div className={s.hideDesktop}>
+          <h3 className={s.text}>Особливості поведінки</h3>
+          <p className={s.description}>{pet.description}</p>
+          <button className={s.button}>
+            <span>Усиновити</span>
+            <svg className={s.iconHeart}>
+              <use href={`${sprite}#icon-pets-heart`} />
+            </svg>
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
